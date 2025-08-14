d2d9855180b0c856f02149678f7f4df1eccc68d3
bu <eric.bu@etu.u-paris.fr>
2025-08-14 15:31:44 +0200

    Correction chemin pour le dossier data

--- scripts/fetch_weather.py
+++ scripts/fetch_weather.py
@@ -1,10 +1,11 @@
 import requests
 import pandas as pd
 from datetime import datetime
+import os
 
 url = "https://api.open-meteo.com/v1/forecast"
-
-params = {"latitude": 48.85, "longitude": 2.35, "hourly": "temperature_2m"}  # paris
+params = {"latitude": 48.85, "longitude": 2.35, "hourly": "temperature_2m"}  # Paris
+os.makedirs("../data", exist_ok=True)  # Création du dossier
 
 r = requests.get(url, params)
 data = r.json()
@@ -16,6 +17,6 @@ df = pd.DataFrame(
 
 # Sauvegarde csv
 today = datetime.now().strftime("%d-%m-%Y")
-output_file = f"./data/weather_data_{today}.csv"
+output_file = f"../data/weather_data_{today}.csv"
 df.to_csv(output_file, index=False)
 print(f"Données sauvegardées dans {output_file}")
